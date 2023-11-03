5ea49c2892cf5060a1940abb9638fcb2a523a2be
mohamedhichy11 <mohamedhichy25@gmail.com>
2023-11-03 20:49:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import Add_to_list from './dotolist/todolist';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Add_to_list />
 }
-
 export default App;
